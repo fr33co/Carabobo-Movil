6d9b07402d168db00103d5e212db056ff044f3c8
JMEspiz <josemespiz@gmail.com>
2015-08-06 08:59:19 -0430

    Validando imagenes .png

--- www/js/filter.js
+++ www/js/filter.js
@@ -2,7 +2,7 @@ angular.module('starter.filter', [])
 
 	.filter('urlFilter', function(){
 		return function(texto){
-			var rex = /http:\/\/carabobo.gob.ve\/wp-content\/uploads\/[0-9]{4}\/[0-9]{2}\/[%a-zA-Z0-9!-]*.jpg/im ;
+			var rex = /http:\/\/carabobo.gob.ve\/wp-content\/uploads\/[0-9]{4}\/[0-9]{2}\/[%a-zA-Z0-9!-]*.[jpgne]{3}/im ;
 			var  retorno = texto.match(rex).toString();
 			return retorno;
 	}})
